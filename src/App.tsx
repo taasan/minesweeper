6fd44a4634b4b78d6a8b12f749134a73e29b0dc6
Trond Aasan <trond.aasan@gmail.com>
2020-03-28 21:46:03 +0100

    Hex startxs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import './App.css';
-import Minesweeper, { LEVELS } from './Components/Minesweeper';
+// import Minesweeper, { LEVELS } from './Components/Minesweeper';
 import StyleSelector from './Components/StyleSelector';
+import SvgMinesweeper, { LEVELS } from './Components/SvgMinesweeper';
+import Minesweeper from './Components/Minesweeper';
+import { hexagonPoints, hexOffset } from './Components';
+
+const level = LEVELS.BEGINNER;
 
 function App() {
+  // viewBox="0 0 2 2"
+  const { rows, cols } = level;
+  const offset = hexOffset;
   return (
     <div className="App">
       <div style={{ position: 'fixed', top: 0, right: 0 }}>
         <StyleSelector />
       </div>
-      <Minesweeper level={LEVELS.BEGINNER} />
+      <svg
+        viewBox={`0 0 ${cols * 2} ${cols * 2}`}
+        width={cols * 2 * 32}
+        height={rows * 2 * 32}
+      >
+        {[...new Array(rows)].map((_a, row) => {
+          return [...new Array(cols)].map((_b, col) => {
+            const x1 = offset * col * 2 + ((row & 1) === 1 ? offset : 0);
+            const y1 = offset * row * Math.sqrt(3);
+
+            return (
+              <g key={`${row}x${col}`}>
+                <polygon
+                  points={[...hexagonPoints()]
+                    .map(({ x, y }) => `${y + x1},${x + y1}`)
+                    .join(' ')}
+                  style={{
+                    fill: 'lime',
+                    strokeWidth: 1,
+                  }}
+                />
+              </g>
+            );
+          });
+        })}
+      </svg>
+      {Math.random() > 0.5 ? (
+        <SvgMinesweeper level={level} />
+      ) : (
+        <Minesweeper level={level} />
+      )}
     </div>
   );
 }
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -6,3 +6,14 @@ export const onContextMenu = (e: {
   e.preventDefault();
   return false;
 };
+
+export const hexOffset = Math.sqrt(3) / 2;
+
+export const hexagonPoints = () => [
+  { x: 0, y: 1 },
+  { x: 0.5, y: hexOffset + 1 },
+  { x: 1.5, y: hexOffset + 1 },
+  { x: 2, y: 1 },
+  { x: 1.5, y: -hexOffset + 1 },
+  { x: 0.5, y: -hexOffset + 1 },
+];
