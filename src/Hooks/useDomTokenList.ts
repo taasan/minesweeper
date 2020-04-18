fc816c72d140a8df94479db12150e96daeed86b9
Trond Aasan <trond.aasan@gmail.com>
2020-04-18 17:43:21 +0200

    Sett element til optional og støtt array i tillegg til varargs

--- src/Hooks/useDomTokenList.ts
+++ src/Hooks/useDomTokenList.ts
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
 
-const useDomTokenList = (element: HTMLElement, ...tokens: string[]) => {
+const useDomTokenList = (
+  element?: HTMLElement,
+  ...tokens: string[] | string[][]
+) => {
+  if (tokens[0] instanceof Array) {
+    tokens = tokens[0];
+  }
   // FIXME: Not sure if this the best way
   // We must trick React to prevent re-render every time
   // Values are compared by === so we must convert to string first
   const json = JSON.stringify(tokens);
-
   useEffect(() => {
-    const list = JSON.parse(json);
+    if (element == null) {
+      return;
+    }
+    const list: string[] = JSON.parse(json);
     // Set up
     element.classList.add(...list);
     // Clean up
