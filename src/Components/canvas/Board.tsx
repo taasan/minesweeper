4fd2c5c207f7bce817dff5d4c2ce85fc3d7c6865
Trond Aasan <trond.aasan@gmail.com>
2020-05-08 23:03:18 +0200

    Fjern ubrukt kode

--- src/Components/canvas/Board.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Level } from '../../Game';
-
-const Board: React.FC<{ level: Level }> = () => {
-  return <canvas></canvas>;
-};
-
-export default Board;
--- src/Components/canvas/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Minesweeper } from './Board';
-export * from './Board';
