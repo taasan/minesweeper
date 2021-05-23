edaed1632bbaf29c607a6ad4542c997df6ed077e
taasan <taasan@users.noreply.github.com>
2021-05-23 09:17:56 +0000

    deploy: 0a19d448d08621a1742fa2270652fa8f4866bfb0

---
+++ precache-manifest.dc5f2bfa5a796af12f37408c5cd4708f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee1063b82da9aef7dfb4ae431e8f02a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "450624a8ea620823fe80",
+    "url": "./static/css/2.5ea632f0.chunk.css"
+  },
+  {
+    "revision": "d261cc7a3ecbfc356425",
+    "url": "./static/css/main.67455c27.chunk.css"
+  },
+  {
+    "revision": "450624a8ea620823fe80",
+    "url": "./static/js/2.4eddf756.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "./static/js/2.4eddf756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d261cc7a3ecbfc356425",
+    "url": "./static/js/main.29f8b609.chunk.js"
+  },
+  {
+    "revision": "ce7f5c97514dd873b59e",
+    "url": "./static/js/runtime-main.eb53abfc.js"
+  },
+  {
+    "revision": "bdf98f4cb1c8cf6a07e30ead076e0f67",
+    "url": "./static/media/symbols.bdf98f4c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minesweeper/precache-manifest.2d138159313144006cec8075fa130c5a.js"
+  "./precache-manifest.dc5f2bfa5a796af12f37408c5cd4708f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minesweeper/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
